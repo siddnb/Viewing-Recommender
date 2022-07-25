da6e0ec12cd7a285c0fcfdac4f980036c311e24b
siddnb <siddhanthab1@gmail.com>
2022-07-25 23:28:41 +0400

    Checkbox functionality for genres working

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormArray, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { RecommenderService } from '../recommender.service';
+
 
 
 @Component({
@@ -10,21 +14,65 @@ import { FormBuilder } from '@angular/forms';
 export class FilterComponent implements OnInit {
 
   filtersForm = this.fb.group({
-    TVorMovie: [''],
-    Genre: [''],
-    Rating: [''],
-    Runtime: [''],
-    ReleaseDate: ['']
+    TVorMovie: '',
+    Genre: this.fb.array([]),
+    Rating: '',
+    Runtime: '',
+    ReleaseDate: ''
   });
 
-  constructor(private fb: FormBuilder) { }
+  public genres: Array<any> = [
+    {name: 'Action', value: 'action'},
+    {name: "Comedy", value: 'comedy'},
+    {name: "Thriller", value: 'thriller'}
+  ];
+
+  constructor(
+    private fb: FormBuilder, 
+    private rs: RecommenderService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onCheckChange(event: any) {
+    const formArray: FormArray = this.filtersForm.get('Genre') as FormArray;
+  
+    /* Selected */
+    if(event.target.checked){
+      // Add a new control in the arrayForm
+      formArray.push(new FormControl(event.target.value));
+    }
+    /* unselected */
+    else{
+      // find the unselected element
+      let i: number = 0;
+  
+      formArray.controls.forEach((ctrl: any) => {
+        if(ctrl.value == event.target.value) {
+          // Remove the unselected element from the arrayForm
+          formArray.removeAt(i);
+          return;
+        }
+  
+        i++;
+      });
+    }
+  }
+
+  private fillOptions(): void{
+    //TODO: error handling/assertions?
+    this.rs.filters.title_type = this.filtersForm.get('TVorMovie')?.value!;
+    this.rs.filters.genres = this.filtersForm.get('Genre')?.value! as string[];
+    this.rs.filters.user_rating = this.filtersForm.get('Rating')?.value!;
+    this.rs.filters.moviemeter = this.filtersForm.get('Runtime')?.value!;
+    this.rs.filters.release_date = this.filtersForm.get('ReleaseDate')?.value!;
+  }
+
   onSubmit(): void{
+    this.fillOptions();
+    this.router.navigateByUrl('/results');
     console.log(this.filtersForm.value);
-    console.log(this.filtersForm.get('TVorMovie')?.value);
   }
 
 }
--- src/app/recommender.service.ts
+++ src/app/recommender.service.ts
@@ -12,13 +12,54 @@ import { Response } from './suggestion';
 export class RecommenderService {
 
   apiKey = environment.apiKey;
-  private filterUrl = `https://imdb-api.com/API/AdvancedSearch/${this.apiKey}?title=The%20godfather&genres=comedy`;
+  private filterUrl = `https://imdb-api.com/API/AdvancedSearch/${this.apiKey}?`;
+
+  filters = {
+    title_type: '',
+    genres: [] as string[],
+    user_rating: '',
+    moviemeter: '',
+    release_date: ''
+  };
 
   constructor(private http: HttpClient) { }
 
+  private createUrl(): string{
+    let query = '';
+
+    for(const [key,value] of Object.entries(this.filters)){
+      if(Array.isArray(value)){
+        let multipleVals = '';
+        for(let i=0; i<value.length; i++){
+          multipleVals += `${value[i]},`;
+        }
+        if(multipleVals!==''){
+          query+=`${key}=${multipleVals}&`;
+        }
+      }
+      else if(value !== '' || value.length!=0){
+        value.trim();
+        //Replace spaces with '%20'
+        value.replace(/\s+/g, '%20');
+        query += `${key}=${value}&`;
+      }
+    }
+
+    //Remove the extraneous '&' at the end of the query
+    //TODO: Make this actually work
+    if(query[-1]==='&'){
+      query.slice(0, query.length - 1);
+    }
+
+    console.log(`this is the query: ${query}`)
+    return query;
+  }
+
   getResults(): Observable<Response>{
     //TODO: Implement error handling
-    return this.http.get<Response>(this.filterUrl).pipe(
+    let query = this.createUrl();
+
+    return this.http.get<Response>(`${this.filterUrl}${query}`).pipe(
       tap(_ => console.log('success')));
   }
 
